9e351dc1aeebd0ba5fa984eb9cff91353179c4d9
Romain Brault <mail@romainbrault.com>
2025-01-10 14:53:21 +0100

    fix: :bug: rework license-file logic

--- src/whiteprints_template_context/package_metadata.py
+++ src/whiteprints_template_context/package_metadata.py
@@ -8,7 +8,12 @@ from importlib import metadata
 from typing import Final
 
 
-__all__: Final = ["__license__", "__license_file__", "__version__"]
+__all__: Final = [
+    "__license__",
+    "__license_file__",
+    "__metadata__",
+    "__version__",
+]
 """Public module attributes."""
 
 __version__: Final = metadata.version(__package__ or "")
@@ -19,7 +24,8 @@ __license__: Final = metadata.metadata(__package__ or "")["License-Expression"]
 
 __license_file__: Final = [
     license_path
-    for license_path in metadata.files(__package__ or "") or ()
-    if (license_path.match("LICENSES/*.txt") and license_path.stem in "MIT")
+    for license_path in (metadata.files(__package__ or "") or [])
+    for license_file in (__metadata__.get_all("License-File") or [])
+    if license_path.match(license_file)
 ]
-"""The package code license file as found by importlib metadata."""
+"""A list containing the path to the license(s) of the package code."""
