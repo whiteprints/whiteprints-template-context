c78be592cbf7ce8b08560ea9f545f27c0ac5ad17
Romain Brault <1455095+RomainBrault@users.noreply.github.com>
2025-01-10 14:59:07 +0100

    fix: :bug: license logic (#55)
    <!--
    # Foreword
    
        We are happy to accept contributions from our users 🚀.
    
        For more details on how to contribute see
    
    [CONTRIBUTING.md](https://github.com/whiteprints/whiteprints-template-context/blob/main/CONTRIBUTING.md).
    
        We follow (and lint) Pull Requests names according to
    [Angular commit
    format](https://gist.github.com/brianclements/841ea7bffdb01346392c#file-commit-formatting-md)
    -->
    
    # Description
    
    Please include a quick summary of the change and which issue is fixed.
    
    Please also include relevant motivation and context.
    
    Fixes # (issue)
    
    # Checklist:
    
      - Quality check
    
    - I agree to follow this project's [Code of
    Conduct](https://github.com/whiteprints/whiteprints-template-context/blob/main/CODE_OF_CONDUCT.md)
    - I have read the [Contributor
    Guide](https://github.com/whiteprints/whiteprints-template-context/blob/main/CONTRIBUTING.md)
        - I have performed a self-review of my own code
        - I have included relevant tests
        - I have commented my code, particularly in hard-to-understand areas
        - I have made corresponding changes to the documentation
    
      - By making a contribution to this project, I certify that:
    
    - The contributor represents and warrants, on behalf of their employer
    or
    other principal if they are acting within the scope of their employment
    or otherwise as the agent of a legal entity, that they have the right
    and
          authority to make their contribution under these terms.
    - The contribution was created in whole or in part by me and I have the
    right to submit it under license of the file(s) modified or created;
          **or**
    - the contribution is based upon previous work that, to the best of my
    knowledge, is covered under an appropriate open source license and I
    have
    the right under that license to submit that work with modifications,
    whether created in whole or in part by me, under the license of the
          file(s) modified or created.

--- src/whiteprints_template_context/package_metadata.py
+++ src/whiteprints_template_context/package_metadata.py
@@ -8,18 +8,27 @@ from importlib import metadata
 from typing import Final
 
 
-__all__: Final = ["__license__", "__license_file__", "__version__"]
+__all__: Final = [
+    "__license__",
+    "__license_file__",
+    "__metadata__",
+    "__version__",
+]
 """Public module attributes."""
 
 __version__: Final = metadata.version(__package__ or "")
 """The package version number as found by importlib metadata."""
+
+__metadata__: Final = metadata.metadata(__package__ or "")
+"""The package metadata."""
 
 __license__: Final = metadata.metadata(__package__ or "")["License-Expression"]
 """The package code license as found by importlib metadata."""
 
 __license_file__: Final = [
     license_path
-    for license_path in metadata.files(__package__ or "") or ()
-    if (license_path.match("LICENSES/*.txt") and license_path.stem in "MIT")
+    for license_path in (metadata.files(__package__ or "") or [])
+    for license_file in (__metadata__.get_all("License-File") or [])
+    if license_path.match(license_file)
 ]
-"""The package code license file as found by importlib metadata."""
+"""A list containing the path to the license(s) of the package code."""
